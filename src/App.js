2a006b340d2ade3e161ecfdfa9679fbb647a2c48
Roy Huaman <RHavila789@gmail.com>
2023-10-02 23:34:13 -0500

    feat: completed tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newGuest: {
+        first_name: "",
+        last_name: "",
+      },
+      guests: [],
+    };
+  }
+
+  handleChangeFirstName = (e) => {
+    this.setState({
+      newGuest: {
+        ...this.state.newGuest,
+        first_name: e.target.value,
+      },
+    });
+  };
+
+  handleChangeLastName = (e) => {
+    this.setState({
+      newGuest: { ...this.state.newGuest, last_name: e.target.value },
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      guests: this.state.guests.concat(this.state.newGuest),
+    });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="first-name"
+                  value={this.state.newGuest.first_name}
+                  onChange={this.handleChangeFirstName}
+                />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="last-name"
+                  value={this.state.newGuest.last_name}
+                  onChange={this.handleChangeLastName}
+                />
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary">
+                  Agregar Invitado
+                </button>
               </div>
             </form>
 
@@ -30,16 +77,19 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.guests.map((guest, index) => (
+                  <tr key={index}>
+                    <td>{guest.first_name}</td>
+                    <td>{guest.last_name}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
-
-
+export default App;
